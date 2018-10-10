9ba060d9caf76ad3a85aeb0bc30fadec75c3bb29
hufanfan <hufanfan@mobike.com>
2018-10-10 19:18:56 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env sh
 
-# 终止一个错误
+# 确保脚本抛出遇到的错误
 set -e
 
-# 构建
+# 生成静态文件
 npm run build
 
-# 进入生成的构建文件夹
+# 进入生成的文件夹
 cd docs/.vuepress/dist
 
-# 如果你是要部署到自定义域名
-# echo 'www.example.com' > CNAME
+# 如果是发布到自定义域名
+echo 'www.hufanfan.com.cn' > CNAME
 
 git init
 git add -A
@@ -19,7 +19,7 @@ git commit -m 'deploy'
 # 如果你想要部署到 https://<USERNAME>.github.io
 git push -f git@github.com:xiaoxiaobaibai/xiaoxiaobaibai.github.io.git master
 
-# 如果你想要部署到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# 如果发布到 https://<USERNAME>.github.io/<REPO>  REPO=github上的项目
+# git push -f git@github.com:xiaoxiaobaibai/vuepress.git master:gh-pages
 
 cd -
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,15 @@
 module.exports = {
-  title: 'Hello VuePress',
-  description: 'Just playing around'
-}
+  title: '个人主页',
+  description: 'Personal Website',
+  head: [ // 注入到当前页面的 HTML <head> 中的标签
+  ],
+  serviceWorker: true, // 是否开启 PWA
+  base: '/', // 部署到github相关的配置
+  markdown: {
+    lineNumbers: true // 代码块是否显示行号
+  },
+  themeConfig: {
+    sidebar: 'auto', // 侧边栏配置
+    sidebarDepth: 2
+  }
+};
